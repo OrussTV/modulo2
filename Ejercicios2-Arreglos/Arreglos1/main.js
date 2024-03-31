4a5bf28e8d9d60dcb361e16b47a940306ad46488
Orlando Algo <oalcantara84@gmail.com>
2024-03-30 20:46:09 -0500

    avance

---
+++ Ejercicios2-Arreglos/Arreglos1/main.js
@@ -0,0 +1,6 @@
+var arreglo = []
+
+ while (arreglo.length <= 10) {
+    arreglo.push(Math.floor(100*Math.random()));
+    console.log(arreglo)
+ }
---
+++ Ejercicios2-Arreglos/Arreglos2/main.js
@@ -0,0 +1,4 @@
+let datos = prompt (`Ingresa datos separados por una ","`)
+datos.constructor.name;                         
+let letters = Array.from(datos.split(","));               
+console.log(letters)
